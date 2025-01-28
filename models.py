491367aaae748cfba632d6fd0e67970299266667
ADITI PRASAD <23f1001093@ds.study.iitm.ac.in>
2025-01-28 16:08:20 +0530

    day-2

--- models.py
+++ models.py
@@ -8,14 +8,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(30),nullable = False, unique = True )
     password = db.Column(db.String(100),nullable = False)
-    full_name = db.Column(db.String(50),nullable = False)
-    qualification = db.Column(db.String(400))
+    qualification = db.Column(db.String(400),nullable = False)
     dob = db.Column(db.Date, nullable = False)
     
     quizez = db.relationship('Score', backref="user", lazy = True)
     
-    is_admin = db.Column(db.Boolean, default=False, nullable=False)
-
+    
 
 class Subject(db.Model):
     __tablename__= 'subject'
--- routes.py
+++ routes.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from app import app
+from models import db, User, Subject, Chapter, Quiz, Question, Option, Score
+from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 @app.route('/')
 def index():
@@ -13,6 +16,37 @@ def login():
 def register():
     return render_template('register.html') 
 
-@app.route('/layout')
-def layout():
-    return render_template('layout.html')
+@app.route('/login', methods=['POST'])
+def login_post():
+    return 'Login post'
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm_password')
+    qualification = request.form.get('qualification')
+    dob = request.form.get('dob')
+    
+    if not username or not password or not confirm_password :
+        flash('Please fill out all required fields.')
+        return redirect(url_for('register'))
+    
+    if password != confirm_password:
+        flash('Passwords do not match.')
+        return redirect(url_for('register'))
+    
+    user = User.query.filter_by(username = username).first()
+    if user:
+        flash('Email address already exists.')
+        return redirect(url_for('register'))
+    
+    dob = datetime.strptime(dob, '%Y-%m-%d').date()
+
+    password_hash = generate_password_hash(password)
+   
+    new_user = User(username=username, password=password_hash, qualification=qualification, dob=dob)
+    db.session.add(new_user)
+    db.session.commit()
+    flash('Registration successful! Please log in.')
+    return redirect(url_for('login'))
